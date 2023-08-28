00146d3276a06263123eec0df11daa59b8baeccc
Rasheeq Zehra <zehrarasheeq@bbdnitm.ac.in>
2023-08-29 01:46:17 +0530

    correct

--- practice2.cpp
+++ practice2.cpp
@@ -4,22 +4,26 @@ class palindrome
 
 {
     public:
-    int pali(int a[], int n)
+    bool pali(int a[], int n)
     {
-        int i, j;
+        int i, j, k;
         string word;
         word= to_string(a[i]);
-        i=0;
-        j=word.length()-1;
+        j=0;
+        k=word.length()-1;
         for(i=0; i<n; i++)
         {
-            if(a[i]!=a[j])
+            while(j<=k)
             {
-                i++;
-                j--;
+            if(word[j]!=word[k])
+            {
+                
                 return false;
             }
+            j++;
+            k--;
         }
+        }    
         return true;
 
     }
@@ -27,9 +31,9 @@ class palindrome
 int main()
 {
     palindrome p;
-    int a[122, 131, 151, 1234];
+    int a[]={123671, 131, 151, 121};
     int n= sizeof (a)/sizeof (a[0]);
-    int result= p.pali(a,n);
+   bool result= p.pali(a,n);
      if (result) {
         cout << "All elements are palindromes." << endl;
     } else {
@@ -39,49 +43,3 @@ int main()
     return 0;
 
 }
-/*
-#include <iostream>
-using namespace std;
-
-class Solution {
-public:
-    bool PalinArray(int a[], int n)
-    {
-        int i, j, k;
-        string word;
-        for (i = 0; i < n; i++)
-        {
-            word = to_string(a[i]);
-            j = word.length() - 1;
-            k = 0;
-            while (k <= j)
-            {
-                if (word[k] != word[j])
-                {
-                    return false;
-                }
-                k++;
-                j--;
-            }
-        }
-        return true; // Return true here if all elements are palindromes
-    }
-};
-
-int main() {
-    Solution solution;
-    
-    int arr[] = {121, 1331, 1221, 1234321};
-    int n = sizeof(arr) / sizeof(arr[0]);
-    
-    bool result = solution.PalinArray(arr, n);
-    
-    if (result) {
-        cout << "All elements are palindromes." << endl;
-    } else {
-        cout << "Not all elements are palindromes." << endl;
-    }
-    
-    return 0;
-}
-*/
