9881a1865ab4518e1e955e544e556995f35fe1b9
Rasheeq Zehra <zehrarasheeq@bbdnitm.ac.in>
2023-09-08 01:07:59 +0530

    hash

--- map.cpp
+++ map.cpp
@@ -3,11 +3,15 @@ using namespace std;
 int main()
 {
     map <string, int> mpp;
-    mpp.insert('abc', 2);
+    mpp.insert({"abc", 2});
+    mpp.insert({"cde", 3});
+    mpp.insert({"gfg", 4});
+    mpp.insert({"cde", 2});
     for(auto it: mpp)
     {
-        cout<<it.first<<endl;
+        cout<<it.first<<"\t"<<it.second<<"\n"<< endl;
     }
 
 
 }
+
