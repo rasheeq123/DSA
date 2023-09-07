febb556a7d9c8f95779348aaf06efb40e5d38165
Rasheeq Zehra <zehrarasheeq@bbdnitm.ac.in>
2023-09-07 23:06:47 +0530

    map

---
+++ map.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    map <string, int> mpp;
+    mpp ["raj"]= 89;
+    mpp ["raam"]= 29;
+    mpp ["sita"]= 19;
+    mpp ["sita"]= 99;
+    for(auto it: mpp)
+    {
+        cout<<mpp.first;
+    }
+
+
+}
