fe35b73caa6c5eeddab19e398e2168661d871c88
Rasheeq Zehra <zehrarasheeq@bbdnitm.ac.in>
2023-09-10 23:48:01 +0530

    fun

---
+++ add_fuc.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int add(int a, int b)
+{
+    // float sum=a+b;
+    return a+b;
+}
+int main()
+{
+    int p,q;
+    //cout<<add(2,5);
+    cin>>p;
+    cin>>q;
+    cout<<"add:"<<add(p,q);
+
+}
