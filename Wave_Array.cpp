e4c12c7facdc7193c2c51cae96a7a972ae83b785
Rasheeq Zehra <zehrarasheeq@bbdnitm.ac.in>
2023-09-28 22:30:20 +0530

    Wave_Array

--- Wave_Array.cpp
+++ Wave_Array.cpp
@@ -5,7 +5,7 @@ class Solution{
     public:
      void convertToWave(int n, vector<int>& arr){
 for (int i = 0; i < n - 1; i += 2) {
-            // Swap adjacent elements to create a wave pattern
+           
             swap(arr[i], arr[i + 1]);
         }
      }
@@ -13,19 +13,19 @@ for (int i = 0; i < n - 1; i += 2) {
   int main()
 {
     int t,n;
-    cin>>t; //Input testcases
-    while(t--) //While testcases exist
+    cin>>t; 
+    while(t--) 
     {
-        cin>>n; //input size of array
-        vector<int> a(n); //declare vector of size n
+        cin>>n; 
+        vector<int> a(n); /
         for(int i=0;i<n;i++)
-            cin>>a[i]; //input elements of array
+            cin>>a[i]; 
         sort(a.begin(),a.end());
         Solution ob;
         ob.convertToWave(n, a);
 
         for(int i=0;i<n;i++)
-            cout<<a[i]<<" "; //print array
+            cout<<a[i]<<" "; 
             
         cout<<endl;
     }
