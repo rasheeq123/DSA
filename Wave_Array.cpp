b309807d28ab3278528d78205ab835810c027011
Rasheeq Zehra <zehrarasheeq@bbdnitm.ac.in>
2023-09-28 22:30:47 +0530

    WaveArray

--- Wave_Array.cpp
+++ Wave_Array.cpp
@@ -17,7 +17,7 @@ for (int i = 0; i < n - 1; i += 2) {
     while(t--) 
     {
         cin>>n; 
-        vector<int> a(n); /
+        vector<int> a(n); 
         for(int i=0;i<n;i++)
             cin>>a[i]; 
         sort(a.begin(),a.end());
