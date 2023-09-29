11f04a6b7e65512d382f227aa7d8d8da2bf0e9a6
Rasheeq Zehra <zehrarasheeq@bbdnitm.ac.in>
2023-09-29 23:51:11 +0530

    No of Enclaves

--- Enclave.cpp
+++ Enclave.cpp
@@ -1,14 +1,80 @@
-bool increasing = true;  // Assume it's increasing initially
-        bool decreasing = true;  // Assume it's decreasing initially
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    int numberOfEnclaves(vector<vector<int>> &grid) {
+        int rows = grid.size();
+        int cols = grid[0].size();
         
-        for (int i = 1; i < nums.size(); ++i) {
-            if (nums[i] > nums[i - 1]) {
-                decreasing = false;  // If we find an increasing pair, it's not decreasing
-            } else if (nums[i] < nums[i - 1]) {
-                increasing = false;  // If we find a decreasing pair, it's not increasing
+        // Mark all land cells connected to the boundary as visited.
+        for (int i = 0; i < rows; ++i) {
+            if (grid[i][0] == 1) {
+                dfs(grid, i, 0);
+            }
+            if (grid[i][cols - 1] == 1) {
+                dfs(grid, i, cols - 1);
             }
         }
         
-        // If either increasing or decreasing is true, the array is monotonic
-        return increasing || decreasing;
+        for (int j = 0; j < cols; ++j) {
+            if (grid[0][j] == 1) {
+                dfs(grid, 0, j);
+            }
+            if (grid[rows - 1][j] == 1) {
+                dfs(grid, rows - 1, j);
+            }
+        }
+        
+        // Count the remaining land cells that are not connected to the boundary.
+        int count = 0;
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                if (grid[i][j] == 1) {
+                    count++;
+                }
+            }
+        }
+        
+        return count;
     }
+    
+private:
+    void dfs(vector<vector<int>>& grid, int row, int col) {
+        int rows = grid.size();
+        int cols = grid[0].size();
+        
+        if (row < 0 || row >= rows || col < 0 || col >= cols || grid[row][col] == 0) {
+            return;
+        }
+        
+        grid[row][col] = 0; 
+        
+        
+        dfs(grid, row + 1, col);
+        dfs(grid, row - 1, col);
+        dfs(grid, row, col + 1);
+        dfs(grid, row, col - 1);
+    }
+    };
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m;
+        cin >> n >> m;
+        vector<vector<int>> grid(n, vector<int>(m));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                cin >> grid[i][j];
+            }
+        }
+        Solution obj;
+        cout << obj.numberOfEnclaves(grid) << endl;
+    }
+}
+// } Driver Code E
