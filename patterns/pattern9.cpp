e9f51090109342f60c6a6783e24273fa33e26fcb
Rasheeq Zehra <zehrarasheeq@bbdnitm.ac.in>
2024-03-23 23:58:00 +0530

    diamond shape

---
+++ patterns/pattern9.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+void pattern(int n)
+{
+    int j;
+
+    for()
+    for(int i=0; i<n; i++)
+    {
+        for( j=0; j<i; j++)
+        {
+            cout<<" ";
+        }
+        for(j=0; j<2*n-(2*i+1); j++)
+        {
+            cout<<"*";
+        }
+
+        for( j=0; j<i; j++)
+        {
+            cout<<" ";
+        }
+        cout<<endl;
+    }
+}
+int main()
+{
+        
+        pattern(4);
+        return 0;
+}
