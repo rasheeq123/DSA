f3cc18adb1d0f538c01095dec423dddb58845432
Rasheeq Zehra <zehrarasheeq@bbdnitm.ac.in>
2024-03-23 23:10:42 +0530

    Fix pattern

--- patterns/pattern8.cpp
+++ patterns/pattern8.cpp
@@ -5,6 +5,25 @@ void pattern(int n)
     int j;
     for(int i=0; i<n; i++)
     {
-        for(j=0; j<2*n)
+        for( j=0; j<i; j++)
+        {
+            cout<<" ";
+        }
+        for(j=0; j<2*n-(2*i+1); j++)
+        {
+            cout<<"*";
+        }
+
+        for( j=0; j<i; j++)
+        {
+            cout<<" ";
+        }
+        cout<<endl;
     }
+}
+int main()
+{
+        
+        pattern(4);
+        return 0;
 }
