6623b860a640c11cb1dc027efc7dbf417d6df3c7
Rasheeq Zehra <zehrarasheeq@bbdnitm.ac.in>
2024-04-06 23:59:10 +0530

    PS

--- POTD/Count_ways_to_N'th_Stair.cpp
+++ POTD/Count_ways_to_N'th_Stair.cpp
@@ -1,4 +1,5 @@
-//{ Driver Code Starts
+//There are n stairs, and a person standing at the bottom wants to reach the top. The person can climb either 1 stair or 2 stairs at a time. 
+//Count the number of ways, the person can reach the top (order does not matter).
 #include <bits/stdc++.h>
 using namespace std;
 
