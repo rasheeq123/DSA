d5b8bdefb3617c6027016208a2339898ae117325
Rasheeq Zehra <zehrarasheeq@bbdnitm.ac.in>
2024-04-12 23:47:55 +0530

    finding the number of pairs with bitwise OR greater than bitwise AND

---
+++ POTD/potd56789.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
+    long long pairAndSum(int n, long long arr[]) {
+        // code here
+        map<long long ,long long> mp;
+        long long ans = 0;
+        for(int i = 0; i < n; i++)
+        {
+            int j = 0;
+            long long temp = arr[i];
+            while(temp)
+            {
+                if(temp&1)
+                    mp[j]++;
+                j++;
+                temp = temp/2;
+            }
+        }
+        for(int i = 0; i < 32; i++)
+        {
+            long long pairs = ((mp[i]-1) * mp[i]) / 2;
+            ans += pairs * (long long)pow(2,i);
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+        long long Arr[N];
+        for (int i = 0; i < N; i++) {
+            cin >> Arr[i];
+        }
+        Solution ob;
+        cout << ob.pairAndSum(N, Arr) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
