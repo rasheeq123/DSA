a55d8fa22dc60f8d6862f51dcdd27aea72661b45
Rasheeq Zehra <zehrarasheeq@bbdnitm.ac.in>
2024-05-10 23:42:13 +0530

    remove duplicates from a sequence

---
+++ tcs/problem.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <set>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> sequence(n);
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> sequence[i];
+    }
+
+    set<int> seen;
+
+    for (int i = 0; i < n; ++i)
+    {
+        while (seen.count(sequence[i]))
+        {
+            sequence[i]++;
+        }
+        seen.insert(sequence[i]);
+    }
+
+    for (int i = 0; i < n; ++i)
+    {
+        cout << sequence[i] << " ";
+    }
+    cout << endl;
+   
+}
