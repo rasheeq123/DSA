b36cc91ba5ed0b2a315b3854b12cc6b4100fd2d1
Rasheeq Zehra <zehrarasheeq@bbdnitm.ac.in>
2024-05-25 23:44:08 +0530

    Implement max_Books function in  class

---
+++ POTD/potd25.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+#include <algorithm>
+#include <cstdio>
+#include <iostream>
+//#include<Windows.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    /*You are requried to complete this method */
+    long long max_Books(int arr[], int n, int k) {
+        // Your code here
+           int left=0,right=0;
+        long long maxi=0; 
+        long long sum=0; 
+        while(right<n){
+            if(arr[right]>k){
+                right++;
+                left=right;
+                sum=0;
+            }
+            else{
+                sum+=arr[right];
+                right++;
+                maxi=max(maxi,sum);
+            }
+        }
+        return maxi;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, k;
+        cin >> n >> k;
+        int ar[n];
+        for (int i = 0; i < n; i++) {
+            cin >> ar[i];
+        }
+        Solution ob;
+        cout << ob.max_Books(ar, n, k) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
