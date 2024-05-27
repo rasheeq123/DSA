88097bbeb81eaad35206e2305f109661faaf3dec
Rasheeq Zehra <zehrarasheeq@bbdnitm.ac.in>
2024-05-27 23:19:18 +0530

    Implement longestSubseq function in  class

---
+++ POTD/potd27.cpp
@@ -0,0 +1,71 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+class Array {
+  public:
+    template <class T>
+    static void input(vector<T> &A, int n) {
+        for (int i = 0; i < n; i++) {
+            scanf("%d ", &A[i]);
+        }
+    }
+
+    template <class T>
+    static void print(vector<T> &A) {
+        for (int i = 0; i < A.size(); i++) {
+            cout << A[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int longestSubseq(int n, vector<int> &A) {
+        // code here
+        vector<int>dp(n,1);
+       int temp= -1;
+        for(int i=1;i<n;i++){
+            for(int j=0;j<i;j++)
+            {
+                if(abs(A[j]-A[i])==1){
+                    temp= dp[j]+1;
+                    dp[i]= max(temp,dp[i]);
+                }
+            }
+        }
+        int res=-1;
+        for(int i=0;i<n;i++){
+            res=max(res,dp[i]);
+        }
+        return res;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    scanf("%d ", &t);
+    while (t--) {
+
+        int n;
+        scanf("%d", &n);
+
+        vector<int> a(n);
+        Array::input(a, n);
+
+        Solution obj;
+        int res = obj.longestSubseq(n, a);
+
+        cout << res << endl;
+    }
+}
+
+// } Driver Code Ends
