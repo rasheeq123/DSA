3303cd42566dd4b8b372bb7e49c9399bbb70b050
Rasheeq Zehra <zehrarasheeq@bbdnitm.ac.in>
2024-05-30 00:16:38 +0530

    Implement countWays function in class

---
+++ POTD/potd30.cpp
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int countWays(string S1, string S2) {
+        // code here
+           string S=S1;
+    string T=S2;
+    int n=S.size();
+      int m=T.size();
+        int mod=1e9+7;
+      vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+      for(int i=0;i<=n;i++){
+          dp[i][0]=1;
+      }
+      
+      for(int i=1;i<=n;i++){
+          for(int j=1;j<=m;j++){
+               
+                int match=0;
+                int dont=0;
+                if(S[i-1]==T[j-1]){
+                    match=dp[i-1][j-1];
+                }
+                {
+                    dont=dp[i-1][j];
+                }
+                
+                dp[i][j]= (match%mod+dont%mod)%mod;
+          }
+      }
+      return dp[n][m];
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    scanf("%d ", &t);
+    while (t--) {
+
+        string s1;
+        getline(cin, s1);
+
+        string s2;
+        getline(cin, s2);
+
+        Solution obj;
+        int res = obj.countWays(s1, s2);
+
+        cout << res << endl;
+    }
+}
+
+// } Driver Code Ends
