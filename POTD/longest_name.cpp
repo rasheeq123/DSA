fd1aec7ac0f7d19ab4fdf28023d72c287ad1eeb4
Rasheeq Zehra <zehrarasheeq@bbdnitm.ac.in>
2024-06-24 18:34:15 +0530

    feat: Add Solution for finding the longest name in a vector

---
+++ POTD/longest_name.cpp
@@ -0,0 +1,72 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Array
+{
+public:
+    template <class T>
+    static void input(vector<T> &A,int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            cin >> A[i];
+        }
+    }
+
+    template <class T>
+    static void print(vector<T> &A)
+    {
+        for (int i = 0; i < A.size(); i++)
+        {
+            cout << A[i]<< " ";
+        }
+        cout << endl;
+    }
+};
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    string longest(int n, vector<string> &names) {
+        // code here
+       string longest_length=" ";
+       int i;
+        for( i=0; i<n; i++)
+        {
+            if(names[i].length()>longest_length.length())
+            {
+                longest_length=names[i];
+            }
+        }
+        return longest_length;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    scanf("%d ",&t);
+    while(t--){
+        
+        int n;
+        scanf("%d",&n);
+        
+        
+        vector<string> names(n);
+        Array::input(names,n);
+        
+        Solution obj;
+        string res = obj.longest(n, names);
+        
+        cout<<res<<"\n";
+        
+    }
+}
+
+// } Driver Code Ends
