d68b3d486c4defabc162c5dc5f877bce665d99c6
Rasheeq Zehra <zehrarasheeq@bbdnitm.ac.in>
2024-07-09 18:28:20 +0530

    Add solution for finding the closest sum of three elements in an array to a target value

---
+++ POTD/ptd09.cpp
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+
+// arr : given vector of elements
+// target : given sum value
+
+class Solution {
+  public:
+    int threeSumClosest(vector<int> arr, int target) {
+        // Your code goes here
+        int x=arr.size(),min=INT_MAX,max=INT_MIN;
+        map<int,vector<int>> m;
+        sort(arr.begin(),arr.end());
+        for(int i=0;i<x-2;i++){
+            int left=i+1,right=x-1;
+            while(left<right){
+                int sum=arr[i]+arr[left]+arr[right];
+                if(sum==target) return target;
+                else if(sum>target) right--;
+                else left++;
+                if(abs(sum-target)<=min) {min=abs(sum-target);m[min].push_back(sum);}
+            }
+            
+        }
+        for(auto i:m[min]){
+            if(i>max) max=i;
+        }
+        return max;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        cin.ignore();
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        int key;
+        cin >> key;
+        // cin.ignore();
+        // if (f)
+        //     return 0;
+        Solution ob;
+        cout << ob.threeSumClosest(arr, key) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
