39feb978f4f8eb3ad82ab5b0a105c2ed694d704f
Rasheeq Zehra <zehrarasheeq@bbdnitm.ac.in>
2024-08-07 22:46:02 +0530

    Add solution for finding the kth element in two sorted arrays

---
+++ POTD/07.cpp
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int kthElement(int k, vector<int>& arr1, vector<int>& arr2) {
+        // code here
+          std::vector<int> newarray(arr1);
+    newarray.insert(newarray.end(), arr2.begin(), arr2.end());
+
+    // Sort the merged array
+    std::sort(newarray.begin(), newarray.end());
+
+    // Return the k-th element (considering 0-based indexing)
+    k=k-1;
+    if (k >= 0 && k < newarray.size()) {
+        return newarray[k];
+    }
+    }
+};
+
+//{ Driver Code Starts.
+
+// Driver code
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        int n, m, k;
+        cin >> k;
+        cin.ignore();
+        string input;
+        int num;
+        vector<int> arr1, arr2;
+
+        getline(cin, input);
+        stringstream s2(input);
+        while (s2 >> num) {
+            arr1.push_back(num);
+        }
+
+        getline(cin, input);
+        stringstream s3(input);
+        while (s3 >> num) {
+            arr2.push_back(num);
+        }
+
+        Solution ob;
+        cout << ob.kthElement(k, arr1, arr2) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
