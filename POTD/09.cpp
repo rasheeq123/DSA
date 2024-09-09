86528d1661a3aca3f7f396f094ed4da20be769ad
Rasheeq Zehra <zehrarasheeq@bbdnitm.ac.in>
2024-09-09 20:23:43 +0530

    Add Solution class for sorting an array of 0s, 1s, and 2s

---
+++ POTD/09.cpp
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    void sort012(vector<int>& arr) {
+        // code here
+         int n=arr.size();
+        multiset<int> s;
+        for(int i=0;i<n;i++)
+        {
+            s.insert(arr[i]);
+        }
+        int i=0;
+        for(auto it=s.begin();it!=s.end() && i!=n;it++,i++)
+        {
+            arr[i]=*it;
+        }
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+
+        vector<int> a;
+        string input;
+        int num;
+
+        getline(cin, input);
+        stringstream s2(input);
+        while (s2 >> num) {
+            a.push_back(num);
+        }
+        Solution ob;
+        ob.sort012(a);
+
+        int n = a.size();
+        for (int i = 0; i < n; i++) {
+            cout << a[i] << " ";
+        }
+
+        cout << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
